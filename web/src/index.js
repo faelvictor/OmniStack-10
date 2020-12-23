8f513268179b4f6e60b40569e840a9d6c48f0a2d
Rafael Victor <faelvictor@users.noreply.github.com>
2020-12-23 17:44:16 -0300

    OmniStack-10

--- web/src/index.js
+++ web/src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react';          /* Importando o react.
+Importamos o react em todo o arquivo javascript que for utilizar HTML dentro dele.
+O HTML vai estar dentro de um arquivo JS
+JSX = JavaScript + HTML*/
+import ReactDOM from 'react-dom';   /* Importa o ReactDOM.
+O ReactDom da a possibilidade de se comunicar com a árvore de elementos da aplicação (html) */
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render(      // Está 'renderizando' ou injetando o código HTML (<App \>) para dentro da div 'root'
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
