02fab03d2ef9845f475a5222aa501accba430b3e
Rafael Victor <faelvictor@users.noreply.github.com>
2021-01-19 14:12:42 -0300

    Latitude e longitude

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,  useEffect } from 'react';
 
 import './global.css';  /* Importa o global.css */ 
 import './app.css';
@@ -14,31 +14,100 @@ function App() {      /* O nosso App.js é uma função que retorna um conteúdo
   fazer uma outra div, por isso em 'latitude' e 'longitude' há duas divs em torno. Na div pai chamei o 
   'div.input.group'.Outra alternativa é usar grid
   */
- 
+
+  // Estamos guardando as informaçoes do formulário em variáveis do estado do componente
+  // E agora quando formos usar essas variáveis, eu vou ter acesso em tempo real ao oque o usuario digitou 
+  const [github_username, setGithub_username] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  // função para disparar quando o usuario clicar no Submit
+  async function handleSubmit(e) {
+    // Como o submit tem como comportamento padrão enviar o usuario para outra tela, vamos evitar esse comportamento padrão mexendo no evento
+    e.preventDefault(); // previnindo o comportamento padrão do formulário
+
+  }
+
+  // 'navegator.geolocation.getCurrentPosition();'  função do navegador para pegar lati/longi
+  // eu não posso só jogar essa função dentro do App, pois o React tem propriedades/estado, e toda vez
+  // que o estado/propriedade for alterado, a função App executa toda de novo, e não posso deixar isso acontecer.
+  // Então, oque fazer ? Usamos o useEffect do React.
+  // useEffect serve para dispararmos uma função toda vez que uma informação alterar ou uma unica vez durante a rendereização do componente
+  
+  // useEffect: 1º parâmetro: qual função executar, 2º parametro: quando executar. Se for vazio, ele executa 1 vez.
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => { // se deu sucesso
+        const { latitude, longitude} = position.coords;  // pegamos a latitude e longitude de coords
+
+        // Agora precisamos jogar dentro do HTML. Se tivesse usando o HTML comum uriamos usar algo parecido com isso:
+        // document.GetElementoById('laitude).value = latitude;
+        // Isso chama-se programação declarativa, estou declarando oque eu quero
+        // Mas no React usamos um paradigma de programação imperativa
+        // Mas oque é essa programação imperativa? Vou então criar um ESTADO, e meu componente precisa saber comportar baseado no estado
+        // Entao vamos criar um estado para armazenar a lati/longi, com o useState
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {  // se deu erro
+        console.log(err);
+      },
+      { // parâmetros para o getCurrentPosition
+        timeout: 30000,
+      }
+    );
+  }, []); 
+
   return (    // HTML
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
+        <form onSubmit={handleSubmit()}>
+          <div className="input-block">
             <label htmlFor="github_username">Usuário Github</label>
-            <input name="github_username" id="github_username" required></input>
+            <input name="github_username" 
+            id="github_username" 
+            required
+            value={github_username}
+            onChange={e => setGithub_username(e.target.value)}
+            ></input>
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required></input>
+            <input name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+              ></input>
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
             <label htmlFor="latitude">Latitude</label>
-            <input name="latitude" id="latitude" required></input>
+            <input type="number" 
+            name="latitude" 
+            id="latitude" required 
+            value={latitude}
+            onChange={e => setLatitude(e.target.value)} 
+            // Toda vez que mudar o valor da latitude...Vou pegar o EVENTO do HTML e vou setLatitude
+            // target é para pegar o input. Essa é a forma que a gente faz no React de armazenar o valor de um input dentro de um valor no estado
+            // Ou seja, toda vez que o usuario digitar no input, ele vai setar no estado 
+            ></input>
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required></input>
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" required 
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+                ></input>
             </div>
           </div>
           
